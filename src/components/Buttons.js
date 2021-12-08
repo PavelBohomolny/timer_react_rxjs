e69da3d7dd64cf8aaa4bc9e360efe8e605bc6480
NexusUA <62807847+NexusUA@users.noreply.github.com>
2021-12-08 12:47:22 +0200

    Init

---
+++ src/components/Buttons.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Buttons({ start, stop, reset, wait }) {
+  return (
+    <div>
+      <button
+        className={'btn btn-outline-success btn-lg  mybtn start'}
+        onClick={start}>
+        Start
+      </button>
+      <button
+        className={'display wait'}
+        onClick={stop}>
+        Wait
+      </button>
+      <button className="btn btn-outline-info mybtn btn-lg reset" onClick={reset}>
+        Reset
+      </button>
+      <button className="btn btn-outline-info mybtn btn-lg clear" onClick={wait}>
+        Clear
+      </button>
+    </div>
+  );
+}
+
+export default Buttons;
